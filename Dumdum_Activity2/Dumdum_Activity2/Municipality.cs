8468c467fb487fd7dbdc7b42d7625476a024fac3
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2026-01-09 08:52:01 +0800

    COde

---
+++ Dumdum_Activity2/Dumdum_Activity2/Municipality.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_Activity2
+{
+    internal class Municipality //Base Class
+    {
+
+        //Properties
+        public string ProvinceName { get; set; }
+        public double Population { get; set; }
+
+        //Constructor
+        public Municipality(string aprovincename, double apopulation)
+        {
+            ProvinceName = aprovincename;
+            Population = apopulation;
+        }
+
+        //Virtual Method
+        public virtual void DisplayMunicipality()
+        {
+            Console.WriteLine("-----Municipality-----");
+            Console.WriteLine("Province Name: " + ProvinceName);
+            Console.WriteLine("Population: " + Population);
+        }
+    }
+    class NorthCebu : Municipality //Derived Class
+    {
+        //Property
+        public string Barangay { get; set; }
+
+        //Constructor
+        public NorthCebu(string abarangay, string aprovincename, double apopulation)
+        : base(aprovincename, apopulation)
+        {
+            Barangay = abarangay;
+        }
+
+        //Override Method
+        public override void DisplayMunicipality()
+        {
+            Console.WriteLine("-----NorthCebu-----");
+            Console.WriteLine("Province Name: " + ProvinceName);
+            Console.WriteLine("Population: " + Population);
+            Console.WriteLine("Barangay: " + Barangay);
+        }
+    }
+    class CentralCebu : Municipality //Derived Class
+    {
+
+        //Property
+        double PostalCode { get; set; }
+
+        //Constructor
+        public CentralCebu(double apostalcode, string aprovincename, double
+        apopulation) : base(aprovincename, apopulation)
+        {
+            PostalCode = apostalcode;
+        }
+        //Override Method
+        public override void DisplayMunicipality()
+        {
+            Console.WriteLine("-----CentralCebu-----");
+            Console.WriteLine("Province Name: " + ProvinceName);
+            Console.WriteLine("Population: " + Population);
+            Console.WriteLine("PostalCode: " + PostalCode);
+        }
+    }
+}
---
+++ Dumdum_Activity2/Dumdum_Activity2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dumdum_Activity2
+{
+    internal class Program
+    {
+
+        //Name:Christian Benedict P. Dumdum
+        //Section: IT401A
+        //REQUIREMENTS:
+        //Convert your program in Activity 1 into Object-Oriented Programming Approach.Make sure
+        //the class objects will be stored and accessed using an Array.Then, display the details of all objects.
+        //Your program must anticipate incorrect entries and other constraints from the user entry. (OPTIONAL)
+        static void Main(string[] args)
+        {
+            //Declare a single array to hold data
+            Municipality[] municipalities = new Municipality[2];
+
+            Console.WriteLine();//Space
+            Console.WriteLine("Municipalities in Cebu Province:");
+            Console.WriteLine(); //Space
+
+            //Initialize the array with specific municipality objects
+            municipalities[0] = new NorthCebu("AtopAtop", "Bantayan", 180000);
+            municipalities[1] = new CentralCebu(6300, "Bohol", 1412726);
+
+            //loop through each municipality in the array
+            foreach (var municipality in municipalities)
+            {
+
+
+                //Calling the Method
+                municipality.DisplayMunicipality();
+
+                //Add Space Between the Municipalities Listed
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
